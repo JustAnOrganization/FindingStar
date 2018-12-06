bddcdfd1840e85e612bce262e486a9278d634820
Jia <supershrimpy@gmail.com>
2018-12-05 19:09:53 -0600

    delete ignored dirs

--- final/cmake-build-debug/CMakeFiles/FinalProject2.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/FinalProject2"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/FinalProject2.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
