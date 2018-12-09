ee918efc836fb1a7d423e84d2d1c51b13c1a6135
ConstLiu <6174liu@gmail.com>
2018-12-08 21:21:25 -0600

    Revert "Merge branch 'master' of https://github.com/JustAnOrganization/FindingStar"
    This reverts commit 2861eaac9060c24a4395b00644b3deba7df20701, reversing
    changes made to 9f362cdb558e94427ce21ed5577e6590d966093c.

---
+++ final/cmake-build-debug/CMakeFiles/FinalProject.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/shrimp/Documents/courses/CG/final/glad/glad.c" "/Users/shrimp/Documents/courses/CG/final/cmake-build-debug/CMakeFiles/FinalProject.dir/glad/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/sdl2/2.0.8/include/SDL2"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/shrimp/Documents/courses/CG/final/Game.cpp" "/Users/shrimp/Documents/courses/CG/final/cmake-build-debug/CMakeFiles/FinalProject.dir/Game.cpp.o"
+  "/Users/shrimp/Documents/courses/CG/final/ModelImporter.cpp" "/Users/shrimp/Documents/courses/CG/final/cmake-build-debug/CMakeFiles/FinalProject.dir/ModelImporter.cpp.o"
+  "/Users/shrimp/Documents/courses/CG/final/Player.cpp" "/Users/shrimp/Documents/courses/CG/final/cmake-build-debug/CMakeFiles/FinalProject.dir/Player.cpp.o"
+  "/Users/shrimp/Documents/courses/CG/final/Table.cpp" "/Users/shrimp/Documents/courses/CG/final/cmake-build-debug/CMakeFiles/FinalProject.dir/Table.cpp.o"
+  "/Users/shrimp/Documents/courses/CG/final/Utils.cpp" "/Users/shrimp/Documents/courses/CG/final/cmake-build-debug/CMakeFiles/FinalProject.dir/Utils.cpp.o"
+  "/Users/shrimp/Documents/courses/CG/final/main.cpp" "/Users/shrimp/Documents/courses/CG/final/cmake-build-debug/CMakeFiles/FinalProject.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/sdl2/2.0.8/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ final/cmake-build-debug/CMakeFiles/FinalProject2.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FinalProject2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/FinalProject2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
